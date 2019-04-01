82be2bb5a97f11405fac93027b821d9e39fcaebb
Elijah Edmondson <elijah.edmondson@gmail.com>
2019-04-01 10:24:51 -0400

    Identifiers added

--- CD3.groovy
+++ CD3.groovy
@@ -1,14 +1,12 @@
 setImageType('BRIGHTFIELD_H_DAB');
 selectAnnotations();
-runPlugin('qupath.imagej.detect.nuclei.PositiveCellDetection', '{"detectionImageBrightfield": "Hematoxylin OD",  "requestedPixelSizeMicrons": 0.5,  "backgroundRadiusMicrons": 0.0,  "medianRadiusMicrons": 0.0,  "sigmaMicrons": 1.5,  "minAreaMicrons": 10.0,  "maxAreaMicrons": 400.0,  "threshold": 0.2,  "maxBackground": 2.0,  "watershedPostProcess": true,  "excludeDAB": false,  "cellExpansionMicrons": 5.0,  "includeNuclei": true,  "smoothBoundaries": true,  "makeMeasurements": true,  "thresholdCompartment": "Nucleus: DAB OD mean",  "thresholdPositive1": 0.7,  "thresholdPositive2": 0.4,  "thresholdPositive3": 0.6,  "singleThreshold": true}');
+runPlugin('qupath.imagej.detect.nuclei.PositiveCellDetection', '{"detectionImageBrightfield": "Hematoxylin OD",  "requestedPixelSizeMicrons": 0.5,  "backgroundRadiusMicrons": 0.0,  "medianRadiusMicrons": 0.0,  "sigmaMicrons": 1.5,  "minAreaMicrons": 10.0,  "maxAreaMicrons": 400.0,  "threshold": 0.07,  "maxBackground": 2.0,  "watershedPostProcess": true,  "excludeDAB": false,  "cellExpansionMicrons": 5.0,  "includeNuclei": true,  "smoothBoundaries": true,  "makeMeasurements": true,  "thresholdCompartment": "Nucleus: DAB OD mean",  "thresholdPositive1": 0.7,  "thresholdPositive2": 0.4,  "thresholdPositive3": 0.6,  "singleThreshold": true}');
 //saveAnnotationMeasurements('C:/Users/edmondsonef/Desktop/QuPath/Sayers/2018.3.2/CD3/');
 
 
 //SAVE ANNOTATIONS //
 
 def name = getProjectEntry().getImageName() + '.txt'
-//def path = buildFilePath('C:/Users/edmondsonef/Desktop/QuPath/Young/PHL 182801/CD3/', 'annotation results')
-//def path = buildFilePath('P:/archive/PHL/Edmondson/QuPath/Young/PHL 190350/', 'CD3')
 def path = buildFilePath(PROJECT_BASE_DIR, 'CD3')
 mkdirs(path)
 path = buildFilePath(path, name)
--- F480.groovy
+++ F480.groovy
@@ -1,7 +1,7 @@
 setImageType('BRIGHTFIELD_H_DAB');
 selectAnnotations();
-mergeSelectedAnnotations();
-selectAnnotations();
+//mergeSelectedAnnotations();
+//selectAnnotations();
 runPlugin('qupath.imagej.detect.nuclei.PositiveCellDetection', '{"detectionImageBrightfield": "Hematoxylin OD", "requestedPixelSizeMicrons": 0.5, "backgroundRadiusMicrons": 0.0,  "medianRadiusMicrons": 0.0,  "sigmaMicrons": 1.5,  "minAreaMicrons": 20.0,  "maxAreaMicrons": 400.0,  "threshold": 0.06,  "maxBackground": 2.0,  "watershedPostProcess": true,  "excludeDAB": false,  "cellExpansionMicrons": 7.0,  "includeNuclei": true,  "smoothBoundaries": true,  "makeMeasurements": true,  "thresholdCompartment": "Cell: DAB OD mean",  "thresholdPositive1": 0.3,  "thresholdPositive2": 0.4,  "thresholdPositive3": 0.6,  "singleThreshold": true}');
 
 //SAVE ANNOTATIONS //
