7a49bbd41a838b375caf58acd89a54d7d5142739
Elijah Edmondson <elijah.edmondson@gmail.com>
2019-05-22 11:30:59 -0400

    Update PDX Biobank Lung Metastasis.groovy

--- PDX Biobank Lung Metastasis.groovy
+++ PDX Biobank Lung Metastasis.groovy
@@ -5,7 +5,7 @@ setColorDeconvolutionStains('{"Name" : "H&E default", "Stain 1" : "Hematoxylin",
 selectAnnotations();
 runPlugin('qupath.imagej.detect.nuclei.WatershedCellDetection', '{"detectionImageBrightfield": "Hematoxylin OD",  "requestedPixelSizeMicrons": 0.5,  "backgroundRadiusMicrons": 8.0,  "medianRadiusMicrons": 0.0,  "sigmaMicrons": 1.5,  "minAreaMicrons": 10.0,  "maxAreaMicrons": 400.0,  "threshold": 0.1,  "maxBackground": 2.0,  "watershedPostProcess": true,  "cellExpansionMicrons": 5.0,  "includeNuclei": true,  "smoothBoundaries": true,  "makeMeasurements": true}');
 
-runClassifier('C:\\Users\\edmondsonef\\Desktop\\QuPath\\PDX Biobank\\classifiers\\BCM4888.ML.qpclassifier');
+runClassifier('C:\\Users\\edmondsonef\\Desktop\\QuPath\\PDX Biobank\\classifiers\\BCM4888.ML2.qpclassifier');
 
 
 import qupath.lib.objects.PathCellObject
