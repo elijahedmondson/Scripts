4b18914c720e189be6286ad6aa6ff77de865d251
Elijah Edmondson <elijah.edmondson@gmail.com>
2019-08-09 08:41:08 -0400

    Update iNOS.groovy

--- iNOS.groovy
+++ iNOS.groovy
@@ -3,7 +3,7 @@ setColorDeconvolutionStains('{"Name" : "H-DAB default", "Stain 1" : "Hematoxylin
 selectAnnotations();
 mergeSelectedAnnotations();
 selectAnnotations();
-runPlugin('qupath.imagej.detect.nuclei.PositiveCellDetection', '{"detectionImageBrightfield": "Hematoxylin OD",  "requestedPixelSizeMicrons": 0.5,  "backgroundRadiusMicrons": 0,  "medianRadiusMicrons": 0.0,  "sigmaMicrons": 1.5,  "minAreaMicrons": 10.0,  "maxAreaMicrons": 400.0,  "threshold": 0.05,  "maxBackground": 2.0,  "watershedPostProcess": true,  "excludeDAB": false,  "cellExpansionMicrons": 5.0,  "includeNuclei": true,  "smoothBoundaries": true,  "makeMeasurements": true,  "thresholdCompartment": "Cell: DAB OD mean",  "thresholdPositive1": 0.6,  "thresholdPositive2": 0.9,  "thresholdPositive3": 0.9,  "singleThreshold": true}');
+runPlugin('qupath.imagej.detect.nuclei.PositiveCellDetection', '{"detectionImageBrightfield": "Hematoxylin OD",  "requestedPixelSizeMicrons": 0.5,  "backgroundRadiusMicrons": 0,  "medianRadiusMicrons": 0.0,  "sigmaMicrons": 1.5,  "minAreaMicrons": 10.0,  "maxAreaMicrons": 400.0,  "threshold": 0.05,  "maxBackground": 2.0,  "watershedPostProcess": true,  "excludeDAB": false,  "cellExpansionMicrons": 5.0,  "includeNuclei": true,  "smoothBoundaries": true,  "makeMeasurements": true,  "thresholdCompartment": "Cell: DAB OD mean",  "thresholdPositive1": 0.4www,  "thresholdPositive2": 0.9,  "thresholdPositive3": 0.9,  "singleThreshold": true}');
 
 
 //Threshold = 0.4 originally, 0.8 for dark tissues
