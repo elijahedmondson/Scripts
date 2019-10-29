580e2e1939e8485cc7764e9b4d13c743e3c9093d
Elijah Edmondson <elijah.edmondson@gmail.com>
2019-10-29 10:16:56 -0400

    CD45

--- PD-L1.groovy
+++ PD-L1.groovy
@@ -3,7 +3,7 @@ runPlugin('qupath.imagej.detect.tissue.SimpleTissueDetection2', '{"threshold": 2
 selectAnnotations();
 //mergeSelectedAnnotations();
 //selectAnnotations();
-runPlugin('qupath.imagej.detect.cells.PositiveCellDetection', '{"detectionImageBrightfield": "Hematoxylin OD",  "requestedPixelSizeMicrons": 0.5,  "backgroundRadiusMicrons": 0,  "medianRadiusMicrons": 0.0,  "sigmaMicrons": 1.5,  "minAreaMicrons": 10.0,  "maxAreaMicrons": 100.0,  "threshold": 0.02,  "maxBackground": 2.0,  "watershedPostProcess": true,  "excludeDAB": false,  "cellExpansionMicrons": 3.0,  "includeNuclei": true,  "smoothBoundaries": true,  "makeMeasurements": true,  "thresholdCompartment": "Cell: DAB OD mean",  "thresholdPositive1": 0.1,  "thresholdPositive2": 0.3,  "thresholdPositive3": 0.5,  "singleThreshold": false}');
+runPlugin('qupath.imagej.detect.nuclei.PositiveCellDetection', '{"detectionImageBrightfield": "Hematoxylin OD",  "requestedPixelSizeMicrons": 0.5,  "backgroundRadiusMicrons": 0,  "medianRadiusMicrons": 0.0,  "sigmaMicrons": 1.5,  "minAreaMicrons": 10.0,  "maxAreaMicrons": 100.0,  "threshold": 0.02,  "maxBackground": 2.0,  "watershedPostProcess": true,  "excludeDAB": false,  "cellExpansionMicrons": 3.0,  "includeNuclei": true,  "smoothBoundaries": true,  "makeMeasurements": true,  "thresholdCompartment": "Cell: DAB OD mean",  "thresholdPositive1": 0.1,  "thresholdPositive2": 0.3,  "thresholdPositive3": 0.5,  "singleThreshold": false}');
 
 
 //SAVE ANNOTATIONS //
