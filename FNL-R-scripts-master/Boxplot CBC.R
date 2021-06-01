2f428054945531b92fc645fc2903cf90d152495f
Elijah Edmondson <elijah.edmondson@gmail.com>
2021-06-01 14:08:05 -0400

    Plot.updates

---
+++ FNL-R-scripts-master/Boxplot CBC.R
@@ -0,0 +1,168 @@
+library(ggplot2)
+library(gridExtra)
+library(readxl)
+#library(ggpubr)
+
+###Generate Data
+
+### WBC  
+my_mean = aggregate(data$WBC, by=list(data$Group), mean) ; colnames(my_mean)=c("Group" , "mean")
+my_CI = aggregate(data$WBC , by=list(data$Group) , FUN = function(x) t.test(x)$conf.int) ; colnames(my_CI)=c("Group" , "CI")
+my_info = merge(my_mean , my_CI , by.x=1 , by.y=1)
+my_info$CIdiff = ((my_CI$CI[,2] - my_CI$CI[,1])/2)
+my_info$ref.low = c(2.71)
+my_info$ref.hi = c(12.33)
+
+### WBC Plot
+WBC <- ggplot(data) + 
+  scale_y_continuous(name = "White Blood Cells") +
+  geom_errorbar(data = my_info, aes(x = Group, ymin = ref.low, ymax = ref.hi), color = "#f5f5f5", width = 0, size=10) +
+  geom_jitter(aes(x = Group, y = WBC, color = Group), width = 0.1, show.legend=F)+
+  geom_point(data = my_info, aes(x = Group , y = mean), color = "#a9a9a9", size = 2) +
+  geom_errorbar(data = my_info, aes(x = Group, y = CIdiff, ymin = mean - CIdiff, ymax = mean + CIdiff), color = "#a9a9a9", width = 0.3 , size=1) +
+  theme_bw() +
+  theme(axis.text.x=element_text(angle=25,hjust=1)) +
+  theme(axis.title.x=element_blank())
+
+
+### Neutrophils
+my_mean = aggregate(data$NE, by=list(data$Group), mean) ; colnames(my_mean)=c("Group" , "mean")
+my_CI = aggregate(data$NE , by=list(data$Group) , FUN = function(x) t.test(x)$conf.int) ; colnames(my_CI)=c("Group" , "CI")
+my_info = merge(my_mean , my_CI , by.x=1 , by.y=1)
+my_info$CIdiff = ((my_CI$CI[,2] - my_CI$CI[,1])/2)
+my_info$ref.low = c(0.46)
+my_info$ref.hi = c(6.04)
+
+### Nuetrophils plot
+NE <- ggplot(data) + 
+  scale_y_continuous(name = "Neutrophils") +
+  geom_errorbar(data = my_info, aes(x = Group, ymin = ref.low, ymax = ref.hi), color = "#f5f5f5", width = 0, size=10) +
+  geom_jitter(aes(x = Group, y = NE, color = Group), width = 0.1, show.legend=F)+
+  geom_point(data = my_info, aes(x = Group , y = mean), color = "#a9a9a9", size = 2) +
+  geom_errorbar(data = my_info, aes(x = Group, y = CIdiff, ymin = mean - CIdiff, ymax = mean + CIdiff), color = "#a9a9a9", width = 0.3 , size=1) +
+  theme_bw() +
+  theme(axis.text.x=element_text(angle=25,hjust=1)) +
+  theme(axis.title.x=element_blank())
+
+
+### Lymphocyes
+my_mean = aggregate(data$LY, by=list(data$Group), mean) ; colnames(my_mean)=c("Group" , "mean")
+my_CI = aggregate(data$LY , by=list(data$Group) , FUN = function(x) t.test(x)$conf.int) ; colnames(my_CI)=c("Group" , "CI")
+my_info = merge(my_mean , my_CI , by.x=1 , by.y=1)
+my_info$CIdiff = ((my_CI$CI[,2] - my_CI$CI[,1])/2)
+my_info$ref.low = c(1.25)
+my_info$ref.hi = c(6.99)
+
+### Lymphocyes plot
+LY <- ggplot(data) + 
+  scale_y_continuous(name = "Lymphocyes") +
+  geom_errorbar(data = my_info, aes(x = Group, ymin = ref.low, ymax = ref.hi), color = "#f5f5f5", width = 0, size=10) +
+  geom_jitter(aes(x = Group, y = LY, color = Group), width = 0.1, show.legend=F)+
+  geom_point(data = my_info, aes(x = Group , y = mean), color = "#a9a9a9", size = 2) +
+  geom_errorbar(data = my_info, aes(x = Group, y = CIdiff, ymin = mean - CIdiff, ymax = mean + CIdiff), color = "#a9a9a9", width = 0.3 , size=1) +
+  theme_bw() +
+  theme(axis.text.x=element_text(angle=25,hjust=1)) +
+  theme(axis.title.x=element_blank())
+
+### Monocytes 
+my_mean = aggregate(data$MO, by=list(data$Group), mean) ; colnames(my_mean)=c("Group" , "mean")
+my_CI = aggregate(data$MO , by=list(data$Group) , FUN = function(x) t.test(x)$conf.int) ; colnames(my_CI)=c("Group" , "CI")
+my_info = merge(my_mean , my_CI , by.x=1 , by.y=1)
+my_info$CIdiff = ((my_CI$CI[,2] - my_CI$CI[,1])/2)
+my_info$ref.low = c(0)
+my_info$ref.hi = c(1.04)
+
+### Monocytes plot
+MO <- ggplot(data) + 
+  scale_y_continuous(name = "Monocytes") +
+  geom_errorbar(data = my_info, aes(x = Group, ymin = ref.low, ymax = ref.hi), color = "#f5f5f5", width = 0, size=10) +
+  geom_jitter(aes(x = Group, y = MO, color = Group), width = 0.1, show.legend=F)+
+  geom_point(data = my_info, aes(x = Group , y = mean), color = "#a9a9a9", size = 2) +
+  geom_errorbar(data = my_info, aes(x = Group, y = CIdiff, ymin = mean - CIdiff, ymax = mean + CIdiff), color = "#a9a9a9", width = 0.3 , size=1) +
+  theme_bw() +
+  theme(axis.text.x=element_text(angle=25,hjust=1)) +
+  theme(axis.title.x=element_blank())
+
+### Eosinophils
+my_mean = aggregate(data$EO, by=list(data$Group), mean) ; colnames(my_mean)=c("Group" , "mean")
+my_CI = aggregate(data$EO , by=list(data$Group) , FUN = function(x) t.test(x)$conf.int) ; colnames(my_CI)=c("Group" , "CI")
+my_info = merge(my_mean , my_CI , by.x=1 , by.y=1)
+my_info$CIdiff = ((my_CI$CI[,2] - my_CI$CI[,1])/2)
+my_info$ref.low = c(0)
+my_info$ref.hi = c(0.25)
+
+### Eosinophils plot
+EO <- ggplot(data) + 
+  scale_y_continuous(name = "Eosinophils") +
+  geom_errorbar(data = my_info, aes(x = Group, ymin = ref.low, ymax = ref.hi), color = "#f5f5f5", width = 0, size=10) +
+  geom_jitter(aes(x = Group, y = EO, color = Group), width = 0.1, show.legend=F)+
+  geom_point(data = my_info, aes(x = Group , y = mean), color = "#a9a9a9", size = 2) +
+  geom_errorbar(data = my_info, aes(x = Group, y = CIdiff, ymin = mean - CIdiff, ymax = mean + CIdiff), color = "#a9a9a9", width = 0.3 , size=1) +
+  theme_bw() +
+  theme(axis.text.x=element_text(angle=25,hjust=1)) +
+  theme(axis.title.x=element_blank())
+
+### Basophils 
+my_mean = aggregate(data$BA, by=list(data$Group), mean) ; colnames(my_mean)=c("Group" , "mean")
+my_CI = aggregate(data$BA , by=list(data$Group) , FUN = function(x) t.test(x)$conf.int) ; colnames(my_CI)=c("Group" , "CI")
+my_info = merge(my_mean , my_CI , by.x=1 , by.y=1)
+my_info$CIdiff = ((my_CI$CI[,2] - my_CI$CI[,1])/2)
+my_info$ref.low = c(0)
+my_info$ref.hi = c(0.23)
+
+### Basophils plot
+BA <- ggplot(data) + 
+  scale_y_continuous(name = "Basophils") +
+  geom_errorbar(data = my_info, aes(x = Group, ymin = ref.low, ymax = ref.hi), color = "#f5f5f5", width = 0, size=10) +
+  geom_jitter(aes(x = Group, y = BA, color = Group), width = 0.1, show.legend=F)+
+  geom_point(data = my_info, aes(x = Group , y = mean), color = "#a9a9a9", size = 2) +
+  geom_errorbar(data = my_info, aes(x = Group, y = CIdiff, ymin = mean - CIdiff, ymax = mean + CIdiff), color = "#a9a9a9", width = 0.3 , size=1) +
+  theme_bw() +
+  theme(axis.text.x=element_text(angle=25,hjust=1)) +
+  theme(axis.title.x=element_blank())
+
+### Hematocrit
+my_mean = aggregate(data$HCT, by=list(data$Group), mean) ; colnames(my_mean)=c("Group" , "mean")
+my_CI = aggregate(data$HCT , by=list(data$Group) , FUN = function(x) t.test(x)$conf.int) ; colnames(my_CI)=c("Group" , "CI")
+my_info = merge(my_mean , my_CI , by.x=1 , by.y=1)
+my_info$CIdiff = ((my_CI$CI[,2] - my_CI$CI[,1])/2)
+my_info$ref.low = c(36.85)
+my_info$ref.hi = c(50.71)
+
+### Hematocrit plot
+HCT <- ggplot(data) + 
+  scale_y_continuous(name = "Hematocrit") +
+  geom_errorbar(data = my_info, aes(x = Group, ymin = ref.low, ymax = ref.hi), color = "#f5f5f5", width = 0, size=10) +
+  geom_jitter(aes(x = Group, y = HCT, color = Group), width = 0.1, show.legend=F)+
+  geom_point(data = my_info, aes(x = Group , y = mean), color = "#a9a9a9", size = 2) +
+  geom_errorbar(data = my_info, aes(x = Group, y = CIdiff, ymin = mean - CIdiff, ymax = mean + CIdiff), color = "#a9a9a9", width = 0.3 , size=1) +
+  theme_bw() +
+  theme(axis.text.x=element_text(angle=25,hjust=1)) +
+  theme(axis.title.x=element_blank())
+
+### Platelets
+my_mean = aggregate(data$PLT, by=list(data$Group), mean) ; colnames(my_mean)=c("Group" , "mean")
+my_CI = aggregate(data$PLT , by=list(data$Group) , FUN = function(x) t.test(x)$conf.int) ; colnames(my_CI)=c("Group" , "CI")
+my_info = merge(my_mean , my_CI , by.x=1 , by.y=1)
+my_info$CIdiff = ((my_CI$CI[,2] - my_CI$CI[,1])/2)
+my_info$ref.low = c(305.79)
+my_info$ref.hi = c(1444.39)
+
+### Platelets plot
+PLT <- ggplot(data) + 
+  scale_y_continuous(name = "Platelets") + 
+  geom_errorbar(data = my_info, aes(x = Group, ymin = ref.low, ymax = ref.hi), color = "#f5f5f5", width = 0, size=10) +
+  geom_jitter(aes(x = Group, y = PLT, color = Group), width = 0.1, show.legend=F)+
+  geom_point(data = my_info, aes(x = Group , y = mean), color = "#a9a9a9", size = 2) +
+  geom_errorbar(data = my_info, aes(x = Group, y = CIdiff, ymin = mean - CIdiff, ymax = mean + CIdiff), color = "#a9a9a9", width = 0.3 , size=1) +
+  theme_bw() +
+  theme(axis.text.x=element_text(angle=25,hjust=1)) +
+  theme(axis.title.x=element_blank())
+
+
+### Generate Multiplots
+
+tiff("CBC.tiff", units="in", width=7, height=9, res=600)
+grid.arrange(HCT, PLT, WBC, NE, LY, MO, EO, BA, ncol = 2, nrow = 4)
+dev.off()
+
--- FNL-R-scripts-master/Boxplot weights.R
+++ FNL-R-scripts-master/Boxplot weights.R
@@ -3,10 +3,8 @@
 library(ggplot2)
 library(gridExtra)
 library(readxl)
-library(ggpubr)
 
-
-
+data <- read_excel("ADME Tox 198.xlsx", sheet = "Tox 198")
 ###Generate Data
 
 ### Body Weight  
@@ -17,11 +15,11 @@ my_info$CIdiff = ((my_CI$CI[,2] - my_CI$CI[,1])/2)
 
 ### BW Plot
 BW <- ggplot(data) + 
+  geom_jitter(aes(x = Group, y = Weight, color = Group), width = 0.1, size = 2, show.legend=F)+
   geom_point(data = my_info, aes(x = Group , y = mean), color = "grey", size = 2) +
   scale_y_continuous(name = "Body Weight (95% CI)") +
-  #geom_errorbar(data = my_info, aes(x = Group, y = CIdiff, ymin = mean - CIdiff, ymax = mean + CIdiff), color = "grey", width = 0.2 , size=0.5) +
+  geom_errorbar(data = my_info, aes(x = Group, y = CIdiff, ymin = mean - CIdiff, ymax = mean + CIdiff), color = "grey", width = 0.2 , size=0.5) +
   theme_bw() +
-  geom_jitter(aes(x = Group, y = Weight), width = 0.1, size = 2)+
   theme(axis.text.x=element_text(angle=25,hjust=1)) +
   theme(axis.title.x=element_blank(), text = element_text(size = 8))
 
@@ -34,11 +32,11 @@ my_info$CIdiff = ((my_CI$CI[,2] - my_CI$CI[,1])/2)
 
 ### Brain plot
 Brain <- ggplot(data) + 
+  geom_jitter(aes(x = Group, y = data$"Brain % BW", color = Group), width = 0.1, show.legend=F)+
   geom_point(data = my_info, aes(x = Group , y = mean), color = "grey", size = 2) +
   scale_y_continuous(name = "Brain (as % of BW)") + #, limits=c(0, 4)) +
-  #geom_errorbar(data = my_info, aes(x = Group, y = CIdiff, ymin = mean - CIdiff, ymax = mean + CIdiff), color = "grey", width = 0.2 , size=0.5) +
+  geom_errorbar(data = my_info, aes(x = Group, y = CIdiff, ymin = mean - CIdiff, ymax = mean + CIdiff), color = "grey", width = 0.2 , size=0.5) +
   theme_bw() +
-  geom_jitter(aes(x = Group, y = data$'Brain % BW'), width = 0.1, size = 1)+
   theme(axis.text.x=element_text(angle=25,hjust=1)) +
   theme(axis.title.x=element_blank(), text = element_text(size = 8))
 
@@ -51,11 +49,11 @@ my_info$CIdiff = ((my_CI$CI[,2] - my_CI$CI[,1])/2)
 
 ### Heart plot
 Heart <- ggplot(data) + 
+  geom_jitter(aes(x = Group, y = data$'Heart % BW', color = Group), width = 0.1, show.legend=F)+
   geom_point(data = my_info, aes(x = Group , y = mean), color = "grey", size = 2) +
   scale_y_continuous(name = "Heart (as % of BW)") + #, limits=c(0, 2)) +
-  #geom_errorbar(data = my_info, aes(x = Group, y = CIdiff, ymin = mean - CIdiff, ymax = mean + CIdiff), color = "grey", width = 0.2 , size=0.5) +
+  geom_errorbar(data = my_info, aes(x = Group, y = CIdiff, ymin = mean - CIdiff, ymax = mean + CIdiff), color = "grey", width = 0.2 , size=0.5) +
   theme_bw() +
-  geom_jitter(aes(x = Group, y = data$'Heart % BW'), width = 0.1, size = 1)+
   theme(axis.text.x=element_text(angle=25,hjust=1)) +
   theme(axis.title.x=element_blank(), text = element_text(size = 8))
 
@@ -67,11 +65,11 @@ my_info$CIdiff = ((my_CI$CI[,2] - my_CI$CI[,1])/2)
 
 ### Liver plot
 Liver <- ggplot(data) + 
+  geom_jitter(aes(x = Group, y = data$'Liver % BW', color = Group), width = 0.1, show.legend=F)+
   geom_point(data = my_info, aes(x = Group , y = mean), color = "grey", size = 2) +
   scale_y_continuous(name = "Liver (as % of BW)") +
-  #geom_errorbar(data = my_info, aes(x = Group, y = CIdiff, ymin = mean - CIdiff, ymax = mean + CIdiff), color = "grey", width = 0.2 , size=0.5) +
+  geom_errorbar(data = my_info, aes(x = Group, y = CIdiff, ymin = mean - CIdiff, ymax = mean + CIdiff), color = "grey", width = 0.2 , size=0.5) +
   theme_bw() +
-  geom_jitter(aes(x = Group, y = data$'Liver % BW'), width = 0.1, size = 1)+
   theme(axis.text.x=element_text(angle=25,hjust=1)) +
   theme(axis.title.x=element_blank(), text = element_text(size = 8))
 
@@ -83,11 +81,11 @@ my_info$CIdiff = ((my_CI$CI[,2] - my_CI$CI[,1])/2)
 
 ### Lung plot
 Lung <- ggplot(data) + 
+  geom_jitter(aes(x = Group, y = data$'Lung % BW', color = Group), width = 0.1, show.legend=F)+
   geom_point(data = my_info, aes(x = Group , y = mean), color = "grey", size = 2) +
   scale_y_continuous(name = "Lung (as % of BW)") +
-  #geom_errorbar(data = my_info, aes(x = Group, y = CIdiff, ymin = mean - CIdiff, ymax = mean + CIdiff), color = "grey", width = 0.2 , size=0.5) +
+  geom_errorbar(data = my_info, aes(x = Group, y = CIdiff, ymin = mean - CIdiff, ymax = mean + CIdiff), color = "grey", width = 0.2 , size=0.5) +
   theme_bw() +
-  geom_jitter(aes(x = Group, y = data$'Lung % BW'), width = 0.1, size = 1)+
   theme(axis.text.x=element_text(angle=25,hjust=1)) +
   theme(axis.title.x=element_blank(), text = element_text(size = 8))
 
@@ -99,11 +97,11 @@ my_info$CIdiff = ((my_CI$CI[,2] - my_CI$CI[,1])/2)
 
 ### Spleen plot
 Spleen <- ggplot(data) + 
+  geom_jitter(aes(x = Group, y = data$'Spleen % BW', color = Group), width = 0.1, show.legend=F) +
   geom_point(data = my_info, aes(x = Group , y = mean), color = "grey", size = 2) +
   scale_y_continuous(name = "Spleen (as % of BW)") + #, limits=c(0, 2)) +
-  #geom_errorbar(data = my_info, aes(x = Group, y = CIdiff, ymin = mean - CIdiff, ymax = mean + CIdiff), color = "grey", width = 0.2 , size=0.5) +
+  geom_errorbar(data = my_info, aes(x = Group, y = CIdiff, ymin = mean - CIdiff, ymax = mean + CIdiff), color = "grey", width = 0.2 , size=0.5) +
   theme_bw() +
-  geom_jitter(aes(x = Group, y = data$'Spleen % BW'), width = 0.1, size = 1) +
   theme(axis.text.x=element_text(angle=25,hjust=1)) +
   theme(axis.title.x=element_blank(), text = element_text(size = 8))
 
@@ -115,157 +113,41 @@ my_info$CIdiff = ((my_CI$CI[,2] - my_CI$CI[,1])/2)
 
 ### Kidney plot
 Kidney <- ggplot(data) + 
+  geom_jitter(aes(x = Group, y = data$'Kidney % BW', color = Group), width = 0.1, show.legend=F) +
   geom_point(data = my_info, aes(x = Group , y = mean), color = "grey", size = 2) +
   scale_y_continuous(name = "Kidney (as % of BW)") +
-  #geom_errorbar(data = my_info, aes(x = Group, y = CIdiff, ymin = mean - CIdiff, ymax = mean + CIdiff), color = "grey", width = 0.2 , size=0.5) +
+  geom_errorbar(data = my_info, aes(x = Group, y = CIdiff, ymin = mean - CIdiff, ymax = mean + CIdiff), color = "grey", width = 0.2 , size=0.5) +
   theme_bw() +
-  geom_jitter(aes(x = Group, y = data$'Kidney % BW'), width = 0.1, size = 1) +
   theme(axis.text.x=element_text(angle=25,hjust=1)) +
   theme(axis.title.x=element_blank(), text = element_text(size = 8))
 
-### Make multiple plots
-ggarrange(BW, Brain, Spleen, Liver, Lung, Kidney, Heart,
-          labels = c("A", "B", "C", "D", "E", "F", "G"),
-          ncol = 2, nrow = 4)
 
-
-### Allograft Weight
-my_mean = aggregate(data$'Allograft Weight', by=list(data$Group), mean) ; colnames(my_mean)=c("Group" , "mean")
-my_CI = aggregate(data$'Allograft Weight' , by=list(data$Group) , FUN = function(x) t.test(x)$conf.int) ; colnames(my_CI)=c("Group" , "CI")
+### Graft Weight
+my_mean = aggregate(data$'Xenograft Weight', by=list(data$Group), mean) ; colnames(my_mean)=c("Group" , "mean")
+my_CI = aggregate(data$'Xenograft Weight' , by=list(data$Group) , FUN = function(x) t.test(x)$conf.int) ; colnames(my_CI)=c("Group" , "CI")
 my_info = merge(my_mean , my_CI , by.x=1 , by.y=1)
 my_info$CIdiff = ((my_CI$CI[,2] - my_CI$CI[,1])/2)
 
-### Allograft plot
-Allograft <- ggplot(data) + 
+### Graft plot
+Graft <- ggplot(data) + 
+  geom_jitter(aes(x = Group, y = data$'Xenograft Weight', color = Group), width = 0.1, show.legend=F) +
   geom_point(data = my_info, aes(x = Group , y = mean), color = "grey", size = 2) +
-  scale_y_continuous(name = "Allograft Weight") + #, limits=c(0, 10)) +
+  scale_y_continuous(name = "Graft Weight") + #, limits=c(0, 10)) +
   geom_errorbar(data = my_info, aes(x = Group, y = CIdiff, ymin = mean - CIdiff, ymax = mean + CIdiff), color = "grey", width = 0.2 , size=0.5) +
   theme_bw() +
-  geom_jitter(aes(x = Group, y = data$'Allograft Weight'), width = 0.1, size = 1) +
   theme(axis.text.x=element_text(angle=25,hjust=1)) +
   theme(axis.title.x=element_blank(), text = element_text(size = 8))
 
 
 ### Make multiple plots
-ggarrange(BW, Allograft, Brain, Spleen, Liver, Lung, Kidney, Heart,
-          labels = c("A", "B", "C", "D", "E", "F", "G", "H"),
-          ncol = 2, nrow = 4)
+tiff("Organ Weights.tiff", units="in", width=7, height=9, res=600)
+grid.arrange(BW, Graft, Brain, Spleen, Liver, Lung, Kidney, Heart,
+             ncol = 2, nrow = 4)
 
-
-
-
-### Make multiple plots
-ggarrange(BW, Spleen, Liver, Lung, Kidney, Heart,
-          labels = c("A", "B", "C", "D", "E", "F"),
-          ncol = 2, nrow = 3)
+dev.off()
 
 
 
 
 
 
-
-
-
-
-
-
-######
-
-
-library(ggplot2)
-library(gridExtra)
-library(readxl)
-library(ggpubr)
-
-
-
-###Generate Data
-
-### Body Weight  
-my_mean = aggregate(data$Weight, by=list(data$Group), mean) ; colnames(my_mean)=c("Group" , "mean")
-
-### BW Plot
-BW <- ggplot(data) + 
-  scale_y_continuous(name = "Body Weight (95% CI)") +
-  theme_bw() +
-  geom_jitter(aes(x = Group, y = Weight), width = 0.1, size = 4)+
-  theme(axis.title.x=element_blank(), text = element_text(size = 8))
-
-
-### Brain Weight
-my_mean = aggregate(data$'Brain % BW', by=list(data$Group), mean) ; colnames(my_mean)=c("Group" , "mean")
-
-### Brain plot
-Brain <- ggplot(data) + 
-  scale_y_continuous(name = "Brain (as % of BW)") + #, limits=c(0, 4)) +
-  theme_bw() +
-  geom_jitter(aes(x = Group, y = data$'Brain % BW'), width = 0.1, size = 1)+
-  theme(axis.title.x=element_blank(), text = element_text(size = 8))
-
-
-### Heart Weight
-my_mean = aggregate(data$'Heart % BW', by=list(data$Group), mean) ; colnames(my_mean)=c("Group" , "mean")
-
-### Heart plot
-Heart <- ggplot(data) + 
-  scale_y_continuous(name = "Heart (as % of BW)") + #, limits=c(0, 2)) +
-  theme_bw() +
-  geom_jitter(aes(x = Group, y = data$'Heart % BW'), width = 0.1, size = 1)+
-  theme(axis.title.x=element_blank(), text = element_text(size = 8))
-
-### Liver Weight
-my_mean = aggregate(data$'Liver % BW', by=list(data$Group), mean) ; colnames(my_mean)=c("Group" , "mean")
-
-### Liver plot
-Liver <- ggplot(data) + 
-  scale_y_continuous(name = "Liver (as % of BW)") +
-  theme_bw() +
-  geom_jitter(aes(x = Group, y = data$'Liver % BW'), width = 0.1, size = 1)+
-  theme(axis.title.x=element_blank(), text = element_text(size = 8))
-
-### Lung Weight
-my_mean = aggregate(data$'Lung % BW', by=list(data$Group), mean) ; colnames(my_mean)=c("Group" , "mean")
-
-### Lung plot
-Lung <- ggplot(data) + 
-  scale_y_continuous(name = "Lung (as % of BW)") +
-  theme_bw() +
-  geom_jitter(aes(x = Group, y = data$'Lung % BW'), width = 0.1, size = 1)+
-  theme(axis.title.x=element_blank(), text = element_text(size = 8))
-
-### Spleen Weight
-my_mean = aggregate(data$'Spleen % BW', by=list(data$Group), mean) ; colnames(my_mean)=c("Group" , "mean")
-
-### Spleen plot
-Spleen <- ggplot(data) + 
-  scale_y_continuous(name = "Spleen (as % of BW)") + #, limits=c(0, 2)) +
-  theme_bw() +
-  geom_jitter(aes(x = Group, y = data$'Spleen % BW'), width = 0.1, size = 1) +
-  theme(axis.title.x=element_blank(), text = element_text(size = 8))
-
-### Kidney Weight
-my_mean = aggregate(data$'Kidney % BW', by=list(data$Group), mean) ; colnames(my_mean)=c("Group" , "mean")
-
-### Kidney plot
-Kidney <- ggplot(data) + 
-  scale_y_continuous(name = "Kidney (as % of BW)") +
-  theme_bw() +
-  geom_jitter(aes(x = Group, y = data$'Kidney % BW'), width = 0.1, size = 1) +
-  theme(axis.title.x=element_blank(), text = element_text(size = 8))
-
-### Mesenteric LN Weight
-my_mean = aggregate(data$'Mesenteric LN % BW', by=list(data$Group), mean) ; colnames(my_mean)=c("Group" , "mean")
-
-### Mesenteric LN  plot
-Mesenteric <- ggplot(data) + 
-  scale_y_continuous(name = "Mesenteric LN % BW") + #, limits=c(0, 10)) +
-  theme_bw() +
-  geom_jitter(aes(x = Group, y = data$'Mesenteric LN % BW'), width = 0.1, size = 1) +
-  theme(axis.title.x=element_blank(), text = element_text(size = 8))
-
-
-### Make multiple plots
-ggarrange(BW, Mesenteric, Brain, Spleen, Liver, Lung, Kidney, Heart,
-          labels = c("A", "B", "C", "D", "E", "F", "G", "H"),
-          ncol = 2, nrow = 4)
