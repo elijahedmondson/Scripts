b4e4a21a6c163a1b7883f0538a873771966ba813
Elijah Edmondson <elijah.edmondson@gmail.com>
2021-06-17 12:32:07 -0400

    Immune.cell

--- 0.2.3 FL Colocalization.groovy
+++ 0.2.3 FL Colocalization.groovy
@@ -1,10 +1,13 @@
-//setChannelNames(
-//      'DAPI',
-//     'GFP-488',
-//     'NeuN-594')
-     
+setImageType('FLUORESCENCE');
+setChannelNames('DAPI','NUMA1','Siglec-15')
 selectAnnotations();
+mergeSelectedAnnotations();     
+selectAnnotations();
+//selectTMACores();
 
-runPlugin('qupath.imagej.detect.cells.WatershedCellDetection', '{"detectionImage": "DAPI",  "requestedPixelSizeMicrons": 0.5,  "backgroundRadiusMicrons": 0.0,  "medianRadiusMicrons": 0.0,  "sigmaMicrons": 0.5,  "minAreaMicrons": 10.0,  "maxAreaMicrons": 100.0,  "threshold": 3000.0,  "watershedPostProcess": true,  "cellExpansionMicrons": 5.0,  "includeNuclei": true,  "smoothBoundaries": true,  "makeMeasurements": true}');
+runPlugin('qupath.imagej.detect.cells.WatershedCellDetection', '{"detectionImage": "DAPI",  "requestedPixelSizeMicrons": 0.5,  "backgroundRadiusMicrons": 0.0,  "medianRadiusMicrons": 0.0,  "sigmaMicrons": 1.5,  "minAreaMicrons": 10.0,  "maxAreaMicrons": 400.0,  "threshold": 10000.0,  "watershedPostProcess": true,  "cellExpansionMicrons": 5.0,  "includeNuclei": true,  "smoothBoundaries": true,  "makeMeasurements": true}');
 
-runObjectClassifier("CD8.IFN")
+//runObjectClassifier("NUMA1.Siglec-15")
+runObjectClassifier("NUMA1.Siglec-15.05")
+//runObjectClassifier("NUMA1.Siglec-15.1")
+//runObjectClassifier("CD4.CD8")
