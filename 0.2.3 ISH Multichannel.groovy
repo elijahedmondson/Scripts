1b7dea849c6da64b66b07d5f4b973695e7c3c0c5
Elijah Edmondson <elijah.edmondson@gmail.com>
2021-07-08 13:34:31 -0400

    Update 0.2.3 ISH Multichannel.groovy

--- 0.2.3 ISH Multichannel.groovy
+++ 0.2.3 ISH Multichannel.groovy
@@ -8,7 +8,7 @@ runPlugin('qupath.imagej.detect.cells.WatershedCellDetection', '{"detectionImage
 
 runPlugin('qupath.imagej.detect.cells.SubcellularDetection', '{"detection[Channel 1]": -1,  "detection[Channel 2]": 21000.0,  "detection[Channel 3]": 8000.0,  "doSmoothing": false,  "splitByIntensity": true,  "splitByShape": true,  "spotSizeMicrons": 1.0,  "minSpotSizeMicrons": 0.1,  "maxSpotSizeMicrons": 100.0,  "includeClusters": true}');
 
-setCellIntensityClassifications("Subcellular: Channel 2: Num spots estimated", 1, 10, 20)
+
 
 
 
@@ -22,14 +22,14 @@ resetDetectionClassifications()
 cells.each {it.setPathClass(getPathClass('Negative'))}
 
 // Get channel 1 & 2 positives
-def ch1Pos = cells.findAll {measurement(it, "Subcellular: Channel 2: Num spots estimated") > 2}
+def ch1Pos = cells.findAll {measurement(it, "Subcellular: Channel 2: Num spots estimated") > 1}
 ch1Pos.each {it.setPathClass(getPathClass('TLR2'))}
 
-def ch2Pos = cells.findAll {measurement(it, "Subcellular: Channel 3: Num spots estimated") > 2}
+def ch2Pos = cells.findAll {measurement(it, "Subcellular: Channel 3: Num spots estimated") > 1}
 ch2Pos.each {it.setPathClass(getPathClass('CD206'))}
 
 // Overwrite classifications for double positives
 def doublePos = ch1Pos.intersect(ch2Pos)
-doublePos.each {it.setPathClass(getPathClass('Double positive'))}
+doublePos.each {it.setPathClass(getPathClass('Double Positive'))}
 
 fireHierarchyUpdate()
