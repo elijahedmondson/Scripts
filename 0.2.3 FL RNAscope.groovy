50248c789a85a3df22b9eed3c2a2f00fa50a5755
Elijah Edmondson <elijah.edmondson@gmail.com>
2021-07-27 12:01:15 -0400

    Update 0.2.3 FL RNAscope.groovy

--- 0.2.3 FL RNAscope.groovy
+++ 0.2.3 FL RNAscope.groovy
@@ -1,22 +1,30 @@
 setChannelNames(
       'DAPI',
-     'SynMUT1')
+     'Csf2',
+     'Cebpb')
      
 selectAnnotations();
+mergeSelectedAnnotations();
+selectAnnotations();
+runPlugin('qupath.imagej.detect.cells.WatershedCellDetection', '{"detectionImage": "DAPI",  "requestedPixelSizeMicrons": 0.1,  "backgroundRadiusMicrons": 8.0,  "medianRadiusMicrons": 0.0,  "sigmaMicrons": 1.5,  "minAreaMicrons": 10.0,  "maxAreaMicrons": 400.0,  "threshold": 10.0,  "watershedPostProcess": true,  "cellExpansionMicrons": 7.0,  "includeNuclei": true,  "smoothBoundaries": true,  "makeMeasurements": true}');
+selectAnnotations();
+runPlugin('qupath.imagej.detect.cells.SubcellularDetection', '{"detection[Channel 1]": -1.0,  "detection[Channel 2]": 60000.0,  "detection[Channel 3]": 10000.0,  "doSmoothing": false,  "splitByIntensity": false,  "splitByShape": false,  "spotSizeMicrons": 1.0,  "minSpotSizeMicrons": 0.2,  "maxSpotSizeMicrons": 2.0,  "includeClusters": true}');
 
-//Cytoplasm expansion
-//runPlugin('qupath.imagej.detect.cells.WatershedCellDetection', '{"detectionImage": "DAPI",  "requestedPixelSizeMicrons": 0.2,  "backgroundRadiusMicrons": 0.0,  "medianRadiusMicrons": 0.0,  "sigmaMicrons": 1.5,  "minAreaMicrons": 10.0,  "maxAreaMicrons": 400.0,  "threshold": 5000.0,  "watershedPostProcess": true,  "cellExpansionMicrons": 5.0,  "includeNuclei": true,  "smoothBoundaries": true,  "makeMeasurements": true}');
 
-//Nucleus only
-runPlugin('qupath.imagej.detect.cells.WatershedCellDetection', '{"detectionImage": "DAPI",  "requestedPixelSizeMicrons": 0.2,  "backgroundRadiusMicrons": 0.0,  "medianRadiusMicrons": 0.0,  "sigmaMicrons": 1.5,  "minAreaMicrons": 10.0,  "maxAreaMicrons": 400.0,  "threshold": 5000.0,  "watershedPostProcess": true,  "cellExpansionMicrons": 0.01,  "includeNuclei": true,  "smoothBoundaries": true,  "makeMeasurements": true}');
-runPlugin('qupath.imagej.detect.cells.SubcellularDetection', '{"detection[Channel 1]": -1.0,  "detection[Channel 2]": 30000.0,  "doSmoothing": true,  "splitByIntensity": true,  "splitByShape": true,  "spotSizeMicrons": 1.0,  "minSpotSizeMicrons": 0.2,  "maxSpotSizeMicrons": 6.0,  "includeClusters": true}');
+def cells = getCellObjects()
+resetDetectionClassifications()
 
-setCellIntensityClassifications("Subcellular: Channel 2: Num spots estimated", 1, 10, 20)
+cells.each {it.setPathClass(getPathClass('Negative'))}
 
-//Using Positive Cell Detection
-//"MEAN" signal
-//runPlugin('qupath.imagej.detect.cells.PositiveCellDetection', '{"detectionImage": "DAPI",  "requestedPixelSizeMicrons": 0.5,  "backgroundRadiusMicrons": 0.0,  "medianRadiusMicrons": 0.0,  "sigmaMicrons": 1.5,  "minAreaMicrons": 10.0,  "maxAreaMicrons": 400.0,  "threshold": 5000.0,  "watershedPostProcess": true,  "cellExpansionMicrons": 5.0,  "includeNuclei": true,  "smoothBoundaries": true,  "makeMeasurements": true,  "thresholdCompartment": "Cell: SynMUT1 mean",  "thresholdPositive1": 8000.0,  "thresholdPositive2": 12000.0,  "thresholdPositive3": 15000.0,  "singleThreshold": false}');
+// Get channel 1 & 2 positives
+def ch2Pos = cells.findAll {measurement(it, "Subcellular: Channel 2: Num spots estimated") > 2}
+ch2Pos.each {it.setPathClass(getPathClass('Csf2 positive'))}
 
-//Using Positive Cell Detection
-//"MAX" signal
-//runPlugin('qupath.imagej.detect.cells.PositiveCellDetection', '{"detectionImage": "DAPI",  "requestedPixelSizeMicrons": 0.5,  "backgroundRadiusMicrons": 0.0,  "medianRadiusMicrons": 0.0,  "sigmaMicrons": 1.5,  "minAreaMicrons": 10.0,  "maxAreaMicrons": 400.0,  "threshold": 6000.0,  "watershedPostProcess": true,  "cellExpansionMicrons": 5.0,  "includeNuclei": true,  "smoothBoundaries": true,  "makeMeasurements": true,  "thresholdCompartment": "Cell: SynMUT1 max",  "thresholdPositive1": 30000.0,  "thresholdPositive2": 40000.0,  "thresholdPositive3": 50000.0,  "singleThreshold": false}');
+def ch3Pos = cells.findAll {measurement(it, "Subcellular: Channel 3: Num spots estimated") > 2}
+ch3Pos.each {it.setPathClass(getPathClass('Cebpb positive'))}
+
+// Overwrite classifications for double positives
+def doublePos = ch2Pos.intersect(ch3Pos)
+doublePos.each {it.setPathClass(getPathClass('Double positive'))}
+
+fireHierarchyUpdate()
